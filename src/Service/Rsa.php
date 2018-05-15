d64090d4a7a094b9ccdafd5ba80016d956f0435d
yanxin1 <yanxin1@staff.sina.com.cn>
2018-05-15 22:09:14 +0800

    新增加密, 调整解密

--- src/Service/Rsa.php
+++ src/Service/Rsa.php
@@ -4,13 +4,25 @@ namespace JROpen\Service;
 
 class Rsa
 {
-    static public function descypt($string, $pri_key, $padding=OPENSSL_PKCS1_PADDING)
+    public static function decrypt($string, $pri_key, $padding = OPENSSL_PKCS1_PADDING)
     {
-        if(openssl_private_decrypt(base64_decode($string), $result, $pri_key, $padding))
-        {
-            return $result;
+        $crypto = '';
+        foreach (str_split(base64_decode($string), 128) as $chunk) {
+            openssl_private_decrypt($chunk, $result, $pri_key);
+            $crypto .= $result;
         }
 
-        return false;
+        return json_decode($crypto, true);
+    }
+
+    public static function encrypt($data, $pub_key, $padding = OPENSSL_PKCS1_PADDING)
+    {
+        $crypto = '';
+        foreach (str_split(json_encode($data), 117) as $chunk) {
+            openssl_public_encrypt($chunk, $result, $pub_key);
+            $crypto .= $result;
+        }
+
+        return base64_encode($crypto);
     }
 }
